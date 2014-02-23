839f19472dfc7d2178a5852101af82ccb82def9d
Shuma Yoshioka <s64.stdio@gmail.com>
2014-02-23 11:44:03 +0900

    confからフィードリストを設定するコード
    Constに入ってるのはよくないけど、とりあえず仮の位置に入れておこう

--- server/src/main/java/co/comugi/feedtokindle/server/Const.java
+++ server/src/main/java/co/comugi/feedtokindle/server/Const.java
@@ -1,7 +1,45 @@
 package co.comugi.feedtokindle.server;
 
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Properties;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
 import co.comugi.feedtokindle.lib.LibConst;
 
 public class Const extends LibConst {
 
+	private static final Properties propConst;
+	private static final Properties propConf;
+	
+	public static final List<String> FEEDS;
+	
+	static {
+		{
+			propConst = new Properties();
+			propConf  = new Properties();
+		}
+		try {
+			propConst.load( Const.class.getResourceAsStream("/properties/const.properties") );
+			propConf.load( Const.class.getResourceAsStream("/properties/conf.properties") );
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		{
+			final String str = propConf.getProperty("feed_list_json","[]");
+			Type type = new TypeToken<List<String>>(){}.getType();
+			List<String> urls = new Gson().fromJson(str,type);
+			if( urls != null )
+				FEEDS = urls;
+			else
+				FEEDS = new ArrayList<String>();
+		}
+	}
+	
 }
